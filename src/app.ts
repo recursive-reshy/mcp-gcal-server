c66f2a6fbbb96c1cff341d49e314b452629aeb50
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-11-19 20:38:56 +0800

    app.ts: Added MCP server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import dayjs from 'dayjs'
+// MCP
+import mcpServer from './transports/https.js'
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js'
 
 const app = express()
 
@@ -15,6 +18,21 @@ app.get( '/health', ( _: Request, res: Response ) => {
       status: 'healthy',  
       timestamp: dayjs().toISOString(),
     } )
+} )
+
+app.post( '/mcp', async ( req: Request, res: Response ) => {
+  const transport = new StreamableHTTPServerTransport( { 
+    sessionIdGenerator: undefined,
+    enableJsonResponse: true
+  } )
+
+  res.on( 'close', () => {
+    transport.close()
+  } )
+
+  await mcpServer.connect( transport )
+
+  await transport.handleRequest( req, res, req.body )
 } )
 
 export default app
