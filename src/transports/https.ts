08f47628f818a985abc389ef7279b9221e16d817
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-11-20 10:40:44 +0800

    transports/https.ts: Reference repo

--- src/transports/https.ts
+++ src/transports/https.ts
@@ -1,6 +1,7 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { tools } from '../tools/index.js'
 
+// Refer to https://github.com/nspady/google-calendar-mcp/blob/main/src/transports/http.ts for more information on how to create an MCP server
 const server = new McpServer( { 
   name: 'mcp-gcal-server',
   version: '0.1.0'
